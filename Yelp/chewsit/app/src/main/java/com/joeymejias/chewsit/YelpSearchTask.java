c8dbe79ca1479201e944a6a83d952c32bbf6d4fe
Jordan Goldberg <jord.goldberg@gmail.com>
2016-08-17 15:57:38 -0400

    added jobscheduler, made the unlimited recycler better, edited the api calls

---
+++ Yelp/chewsit/app/src/main/java/com/joeymejias/chewsit/YelpSearchTask.java
@@ -0,0 +1,38 @@
+package com.joeymejias.chewsit;
+
+import android.location.Location;
+import android.os.AsyncTask;
+
+import com.yelp.clientlib.entities.Business;
+import com.yelp.clientlib.entities.options.CoordinateOptions;
+
+import java.util.ArrayList;
+
+/**
+ * Created by joshuagoldberg on 8/17/16.
+ */
+
+
+public class YelpSearchTask extends AsyncTask<Double, Void, ArrayList<Business>> {
+
+    @Override
+    protected ArrayList<Business> doInBackground(Double... doubles) {
+
+        Location lastLocation = null;
+
+        while (lastLocation == null) {
+            lastLocation = LocationSingleton.getInstance().getCurrentLocation();
+        }
+
+        LocationSingleton.getInstance().setLastSearchLocation(lastLocation);
+
+        double lat = lastLocation.getLatitude();
+        double lng = lastLocation.getLongitude();
+
+        CoordinateOptions coordinate = CoordinateOptions.builder()
+                .latitude(lat)
+                .longitude(lng)
+                .build();
+        return YelpHelper.getInstance().businessSearch(coordinate, doubles[0]);
+    }
+}
